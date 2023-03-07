8207bb09512e37a5735cde4109355f4908391ce4
Bastien Saidi <saidibastien@gmail.com>
2023-03-07 15:54:24 +0000

    Increased Version Number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='lunii',
-    version='0.1.1a2',
+    version='0.1.1a3',
     description='A Python package for working with Lunii devices, and emulating them',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
